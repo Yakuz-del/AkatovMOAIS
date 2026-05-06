1d529629e3cd03b5bc14649ddc24e145b319aef5
Yakuz-del <yurashivan17@gmail.com>
2026-05-06 23:14:09 +0300

    Add files via upload

---
+++ 6dinam-4.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+using namespace std;
+
+void printArray(int* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int cap = 3;  
+    int size = 0;     
+    int* arr = new int[cap];
+
+    int number;
+    while (true) {
+        cout << "Enter number (0 to exit): ";
+        cin >> number;
+        if (number == 0) {
+            break;
+        }
+
+        if (size == cap) {
+          
+            int newCap = cap + 2;
+            int* newArr = new int[newCap];
+
+            for (int i = 0; i < size; i++) {
+                newArr[i] = arr[i];
+            }
+
+
+            delete[] arr;
+
+        
+            arr = newArr;
+            cap = newCap;
+
+            cout << "Expanding to " << cap << " elements" << endl;
+        }
+
+
+        arr[size] = number;
+        size++;
+    }
+
+ 
+  
+    printArray(arr, size);
+
+    delete[] arr;
+    arr = nullptr;
+
+    return 0;
+}
