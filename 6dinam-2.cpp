7c44e9961e6ab4c3d1c9b7cf704da264ceee157f
Yakuz-del <yurashivan17@gmail.com>
2026-05-06 22:42:47 +0300

    Add files via upload

---
+++ 6dinam-2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter array size: ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Array size error" << endl;
+        return 1;
+    }
+
+    int* arr = new int[n];
+
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = i + 1; 
+    }
+
+    cout << "Massiv: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i];
+        if (i != n - 1) {  
+            cout << " ";
+        }
+    }
+    cout << endl;
+
+
+    delete[] arr; 
+    arr = nullptr;
+
+    return 0;
+}
